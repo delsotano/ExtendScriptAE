26ad28b7bd385b1940a848b1f304ed6a67729c2d
Javi Ge <javierluisglez@gmail.com>
2018-09-05 00:20:43 +0200

    Add files via upload
    Para Importar

---
+++ import.jsx
@@ -0,0 +1,1 @@
+﻿var proj = app.project;var miComp = proj.activeItem;var numProj = proj.numItems;alert(numProj);var selection = undefined;for (var i = 1; i<= numProj, i+=1;){        if (proj.item(i).name == "luna 02.mov"){    selection = proj.item(i);    if (selection !== undefined) {        miComp.layers.add(selection);    }    }}
